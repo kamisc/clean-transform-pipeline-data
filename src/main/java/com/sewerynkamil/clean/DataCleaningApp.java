9e5685e5078f5fb9c59dd52e31769368c01371fc
Kamil Seweryn <kamil_seweryn@o2.pl>
2023-01-07 16:51:14 +0100

    add cleaning data app

--- src/main/java/com/sewerynkamil/clean/DataCleaningApp.java
+++ src/main/java/com/sewerynkamil/clean/DataCleaningApp.java
@@ -1,16 +1,25 @@
 package com.sewerynkamil.clean;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static com.sewerynkamil.clean.DataCleaning.*;
 
 public class DataCleaningApp {
     public static void main(String[] args) {
-        String s = "SaleTransaction{uuid='7D30065D-6318-4809-5260-A6C90AB574DD',timestamp='10/04/2022 04:26:03',type='colombian',size='GG',price='€3',offer='',discount='',userId=3096,country=ITALY,city='Turin'}";
-
-        Matcher m = Pattern.compile(
-                "SaleTransaction\\{uuid='?([A-Fa-f0-9\\-]+)'?,timestamp='(.*)',type='?(.*?)'?,size='?(.{1,3}?)'?,price='?(.*?)'?,offer='?(.*?)'?,discount='?(.*?)'?,userId=(\\d*),country='?(.*?)'?,city='?(.*?)'?}"
-        ).matcher(s);
-
-        System.out.println(m.matches());
+        String fileName = "data/sales-global.dat";
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            stream
+                .map(applyRegex)
+                .filter(onlyMatchingLines)
+                .map(composeHashMap)
+                .map(properNulls)
+                .filter(discountAndOfferOnlyWithUserId)
+                .forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
